114f04b8e2c1754a4961b4668c9b79657b508fa8
karthick7204 <karthicklimited1234@gmail.com>
2025-02-19 10:22:15 +0530

    setup a router and mongodb

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,15 @@
 import express from "express"
+import dotenv from "dotenv"
+import authRoutes from "./routes/auth.route.js"
+import {connectDB} from "./lib/db.js"
+dotenv.config();
+
 const app =express();
 
-app.listen(5000,()=>{
+app.use("/api/auth",authRoutes)
+const PORT = process.env.PORT
+
+app.listen(PORT,()=>{
     console.log("server is running on port 5000");
+    connectDB();
 })
