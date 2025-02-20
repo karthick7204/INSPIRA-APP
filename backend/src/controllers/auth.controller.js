500cc76405db21e253a54173479cbc0aeb9669da
karthick7204 <karthicklimited1234@gmail.com>
2025-02-21 00:34:36 +0530

    created a cloudinary api

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -48,7 +48,7 @@ export const login = async (req, res) => {
     const { email, password } = req.body;
 
     try {
-        const user = await User.findOne({ email });  // ✅ Fixed incorrect reference
+        const user = await User.findOne({ email }); 
 
         if (!user) {
             return res.status(400).json({ message: "Invalid credentials" });
