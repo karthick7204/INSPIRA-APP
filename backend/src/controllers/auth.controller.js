22fd2db5070aa7e4ac1f016913005d68c7db51b9
karthick7204 <karthicklimited1234@gmail.com>
2025-02-20 23:19:22 +0530

    adding a login and logout function

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -44,10 +44,38 @@ export const signup = async (req, res) => {
     }
 };
 
-export const login = (req, res) => {
-    res.send("login route");
+export const login = async(req, res) => {
+   const {email,password } = req.body
+   try{
+    const user = await user.findOne({email})
+
+    if(!user) {
+        return res.status(400).json({message:"invalid credentials"})
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if(!isPasswordCorrect) {
+        return res.status(400).json({message:"invalid credentials"})
+    }
+    generateToken(user._id,res)
+
+    res.status(200).json({ //send user data back to client
+         _id:user._id,
+         fullName:user.fullName,
+         email: user.email,
+         profilePic: user.profilePic,
+    })
+   }catch (error) {
+    console.log("error in login controller",error.message);
+    res.status(500).json({message:"internal error"})
+   }
 };
 
-export const logout = (req, res) => {
-    res.send("logout route");
+export const logout = async(req, res) => {
+   try{
+      res.cookie("jwt", "",{maxAge:0})
+      res.status(200).json({message:"logged out successfully"});
+   }catch(error){
+      console.log("error in logout controller",error.message);
+   }
 };
