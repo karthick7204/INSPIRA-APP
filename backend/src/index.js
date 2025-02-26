14465f7c81eb2b6e11db1bb4a3af7988645609b5
karthick7204 <karthicklimited1234@gmail.com>
2025-02-26 19:58:46 +0530

    create a message route and controller

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,15 +3,18 @@ import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
 
 import authRoutes from "./routes/auth.route.js"
+import messageRoutes from "./routes/message.route.js"
+
 import {connectDB} from "./lib/db.js"
 dotenv.config();
 
 const app =express();
 
 app.use(express.json());
-app.use(cookieParser());
+app.use(cookieParser()); 
 
 app.use("/api/auth",authRoutes)
+app.use("/api/message",messageRoutes)
 const PORT = process.env.PORT
 
 app.listen(PORT,()=>{
